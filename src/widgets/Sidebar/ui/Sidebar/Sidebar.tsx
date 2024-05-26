b0723a4dbeafdf9dd49cf6fa7be60125df7e660e
Alexey4717 <lehabourne@gmail.com>
2024-05-26 14:18:21 +0800

    feat: add sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Sidebar.module.scss";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+
+interface SidebarProps {
+  className?: string;
+}
+
+export const Sidebar = ({ className }: SidebarProps) => {
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+
+  const handleToggleButton = () => {
+    setCollapsed((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
+        className,
+      ])}
+    >
+      <button onClick={handleToggleButton}>toggle</button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+        {/* LangSwitcher */}
+      </div>
+    </div>
+  );
+};
