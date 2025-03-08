c34b476ae1a4f2be5c0e45656dacbea740963a00
Alexey4717 <lehabourne@gmail.com>
2025-03-09 03:05:37 +1000

    81l: add useModal hook, upgrade drawer

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,85 +1,56 @@
-import {
-    type ReactNode,
-    useState,
-    useRef,
-    useCallback,
-    useEffect,
-} from 'react';
+import { ReactNode } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { classNames, type Mods } from 'shared/lib/classNames/classNames';
-import cls from './Modal.module.scss';
+import { classNames, Mods } from 'shared/lib/classNames/classNames';
+import { useModal } from 'shared/lib/hooks/useModal/useModal';
 import { Overlay } from '../Overlay/Overlay';
 import { Portal } from '../Portal/Portal';
-
-const ANIMATION_DELAY = 300;
+import cls from './Modal.module.scss';
 
 interface ModalProps {
     className?: string;
     children?: ReactNode;
     isOpen?: boolean;
     onClose?: () => void;
-    // Если в модалку будет помещаться асинхронный компонент
     lazy?: boolean;
 }
 
-export const Modal = ({
-    className,
-    children,
-    isOpen,
-    onClose,
-    lazy = false,
-}: ModalProps) => {
+export const Modal = (props: ModalProps) => {
+    const {
+        className,
+        children,
+        isOpen,
+        onClose,
+        lazy,
+    } = props;
+
+    const {
+        close,
+        isClosing,
+        isMounted,
+    } = useModal({
+        animationDelay: 300,
+        onClose,
+        isOpen,
+    });
+
     const { theme } = useTheme();
-    // Это нужно для создания анимации плавного закрытия, т.к. css не справится
-    const [isClosing, setIsClosing] = useState(false);
-    const [isMounted, setIsMounted] = useState(false);
-    const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
-
-    useEffect(() => {
-        if (isOpen) {
-            setIsMounted(true);
-        }
-    }, [isOpen]);
-
-    const closeHandler = useCallback(() => {
-        if (typeof onClose === 'function') {
-            setIsClosing(true);
-            timerRef.current = setTimeout(() => {
-                onClose();
-                setIsClosing(false);
-            }, ANIMATION_DELAY);
-        }
-    }, [onClose]);
-
-    const onKeyDown = useCallback((event: globalThis.KeyboardEvent) => {
-        if (event.key === 'Escape') {
-            closeHandler();
-        }
-    }, [closeHandler]);
-
-    useEffect(() => {
-        // Закрытие модалки на кнопку Esc
-        if (isOpen) {
-            window.addEventListener('keydown', onKeyDown);
-        }
-        return () => {
-            if (timerRef.current) clearTimeout(timerRef.current);
-            window.removeEventListener('keydown', onKeyDown);
-        };
-    }, [isOpen, onKeyDown]);
 
     const mods: Mods = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
     };
 
-    if (lazy && !isMounted) return null;
+    if (lazy && !isMounted) {
+        return null;
+    }
 
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className])}>
-                <Overlay onClick={closeHandler} />
-                <div className={cls.content}>
+            <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
+                <Overlay onClick={close} />
+                <div
+                    className={cls.content}
+                >
                     {children}
                 </div>
             </div>
