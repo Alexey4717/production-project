5bbb436020c917e25db134fb693c522f41cae1a6
Alexey4717 <lehabourne@gmail.com>
2025-04-01 14:41:47 +1000

    115l: examples skipping test, add stubs (fixtures)

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -9,3 +9,21 @@ Cypress.Commands.addAll(profileCommands);
 Cypress.Commands.addAll(articleCommands);
 Cypress.Commands.addAll(commentsCommands);
 Cypress.Commands.addAll(ratingCommands);
+
+// Пример переопределения команд
+// Cypress.Commands.overwrite('intercept', () => {
+//     const FIXTURE_MODE = process.env.FIXTURE_MODE;
+//     const fixtureName = req.METHOD + req.url + hash(req.body);
+//     if (FIXTURE_MODE === 'READ') {
+//         // фикстуры не списываем, а считываем из существующих
+//         // название фикстур можно брать из url или брать hash-сумму из url (чтоб были уникальными)
+//         readFixture(fixtureName);
+//     }
+//     if (FIXTURE_MODE === 'WRITE') {
+//         // Можно получать данные из запроса и на основании этих данных фикстуру из запроса записывать
+//         createFixture(fixtureName, req.body);
+//         // т.е. прогоняем тесты в релизе, происходят запросы за реальными данными, сохраняются в фикстуры,
+//         // а потом, где-нибудь в CI, чтобы сервер не перегружать, используем фикстуры.
+//     }
+//     if (FIXTURE_MODE === 'API') {}
+// });
