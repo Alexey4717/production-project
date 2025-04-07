30081ed57a06bb242ffaaa11075384155e2eb709
Alexey4717 <lehabourne@gmail.com>
2025-04-07 12:08:36 +1000

    142l: add ui design switcher, mutations for change feature flag, settings page

---
+++ src/features/uiDesignSwitcher/ui/UiDesignSwitcher/UiDesignSwitcher.tsx
@@ -0,0 +1,70 @@
+import { useTranslation } from 'react-i18next';
+import { memo, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { ListBox } from '@/shared/ui/redesigned/Popups';
+import { Text } from '@/shared/ui/redesigned/Text';
+import { getFeatureFlag, updateFeatureFlag } from '@/shared/lib/features';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { getUserAuthData } from '@/entities/User';
+import { HStack } from '@/shared/ui/redesigned/Stack';
+import { Skeleton } from '@/shared/ui/redesigned/Skeleton';
+
+interface UiDesignSwitcherProps {
+    className?: string;
+}
+
+export const UiDesignSwitcher = memo((props: UiDesignSwitcherProps) => {
+    const { className } = props;
+    const { t } = useTranslation();
+    const isAppRedesigned = getFeatureFlag('isAppRedesigned');
+    const dispatch = useAppDispatch();
+    const authData = useSelector(getUserAuthData);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const items = [
+        {
+            content: t('Новый'),
+            value: 'new',
+        },
+        {
+            content: t('Старый'),
+            value: 'old',
+        },
+    ];
+
+    const onChange = async (value: string) => {
+        try {
+            if (!authData) {
+                throw new Error('Error authData definition is occurred');
+            }
+            setIsLoading(true);
+            await dispatch(
+                updateFeatureFlag({
+                    userId: authData.id,
+                    newFeatures: {
+                        isAppRedesigned: value === 'new',
+                    },
+                }),
+            ).unwrap();
+        } catch (error) {
+            console.error(error);
+            setIsLoading(false);
+        }
+    };
+
+    return (
+        <HStack>
+            <Text text={t('Вариант интерфейса')} />
+            {isLoading ? (
+                <Skeleton width={100} height={40} />
+            ) : (
+                <ListBox
+                    onChange={onChange}
+                    items={items}
+                    value={isAppRedesigned ? 'new' : 'old'}
+                    className={className}
+                />
+            )}
+        </HStack>
+    );
+});
